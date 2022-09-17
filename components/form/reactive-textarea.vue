029649bff694eeb77ed9674a64b0cccc831c1ccb
Viki Val <viktorko99@gmail.com>
2022-09-17 22:12:33 +0200

    :zap: Reactive textarea

--- components/form/reactive-textarea.vue
+++ components/form/reactive-textarea.vue
@@ -6,8 +6,14 @@
   import { NInput } from 'naive-ui'
   const value = ref('')
   const { $gun } = useNuxtApp()
+  const copy = $gun.get('test').get('paste');
+  copy.on((data) => { value.value = data });
 
+
+  watch(value, (newValue) => {
+    console.log(`x is ${newValue}`)
+    copy.put(newValue)
+  })
+  
   console.log('$gun',$gun)
-
-  // $gun.get('test').put({ value: 'test' })
 </script>
