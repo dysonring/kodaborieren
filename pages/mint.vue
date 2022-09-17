deab60ec85ca4fbaf476fd843cfb3d472831d026
Viki Val <viktorko99@gmail.com>
2022-09-17 22:12:42 +0200

    :zap: the mint page

--- pages/mint.vue
+++ pages/mint.vue
@@ -1,7 +1,11 @@
 <template>
-  <ReactiveTextarea />
+  <div>
+    <n-h2>Collaborative Prompts</n-h2>
+    <ReactiveTextarea />
+  </div>
 </template>
 
 <script lang="ts" setup>
   import ReactiveTextarea from '~~/components/form/reactive-textarea.vue'
+  import { NH2 } from 'naive-ui';
 </script>
