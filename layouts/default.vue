16dfe18284332558d6867029a0498331561bb512
Viki Val <viktorko99@gmail.com>
2022-09-18 09:39:42 +0200

    :zap: turbokkt :))))

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,38 +1,14 @@
 <template>
-  <div class="min-h-full is-flex is-flex-direction-column">
-    <Navbar />
-    <main class="is-flex-grow-1">
-      <section class="section">
-        <div class="container">
-          <Error
-            v-if="$nuxt.isOffline"
-            :has-img="false"
-            error-title="Offline Detected"
-            error-subtitle="Please check your network connections" />
-          <Nuxt v-else />
-        </div>
-      </section>
-    </main>
-    <LazyTheFooterOld />
+  <div>
+    <navbar/>
+    <n-grid x-gap="12" :cols="1">
+      <n-gi>
+        <slot />
+      </n-gi>
+    </n-grid>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
-
-@Component<DefaultLayout>({
-  name: 'DefaultLayout',
-  head() {
-    return {
-      link: [
-        {
-          hid: 'canonical',
-          rel: 'canonical',
-          href: this.$root.$config.baseUrl + this.$route.path,
-        },
-      ],
-    }
-  },
-})
-export default class DefaultLayout extends Vue {}
+<script setup lang="ts">
+import { NGrid, NGi } from 'naive-ui'
 </script>
