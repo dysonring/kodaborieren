43f8560016daa29dbb4e0369634a8440c122a00d
Viki Val <viktorko99@gmail.com>
2022-09-18 03:37:24 +0200

    :zap: just another component that I wrote during insomnia

---
+++ components/form/basic-input.vue
@@ -0,0 +1,19 @@
+<template>
+  <n-input v-model:value="value" :type="type" placeholder="Work on the prompt together" :rows="5" />
+</template>
+
+<script lang="ts" setup>
+  import { NInput } from 'naive-ui'
+
+  const props = defineProps<{
+    isTextArea?: boolean
+  }>()
+
+  const type = computed(() => props.isTextArea ? 'textarea' : 'text')
+  const value = ref('')
+  const emit = defineEmits(['update:modelValue'])
+
+  watch(value, (newValue) => {
+    emit('update:modelValue', newValue)
+  })
+</script>
