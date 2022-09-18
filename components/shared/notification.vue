81edcc470bdb406e5c44a384d865a211fe2c0db9
Viki Val <viktorko99@gmail.com>
2022-09-18 03:22:01 +0200

    :zap: notifications that I wont use

---
+++ components/shared/notification.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-notification-provider>
+    <slot @success="onSuccess" @fail="onFail" />
+  </n-notification-provider>
+</template>
+
+<script lang="ts" setup>
+  import { useNotification, NNotificationProvider } from 'naive-ui';
+
+  const notification = useNotification()
+
+  function notify(type: string, content: string, meta: string, duration = 2500) {
+    notification[type]({
+      content,
+      meta,
+      duration,
+      keepAliveOnHover: true
+    })
+  }
+
+  function onSuccess({ content, meta, duration }: { content: string, meta: string, duration: number }) {
+    notify('success', content, meta, duration)
+  }
+
+  function onFail({ content, meta, duration }: { content: string, meta: string, duration: number }) {
+    notify('success', content, meta, duration)
+  }
+
+</script>
