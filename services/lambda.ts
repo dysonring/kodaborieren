6fcb7a0e89dd37a38b1874cb21c4617fc4d7b646
Viki Val <viktorko99@gmail.com>
2022-09-18 14:00:43 +0200

    :zap: correct lambdas

--- services/lambda.ts
+++ services/lambda.ts
@@ -1,15 +1,17 @@
 import { $fetch } from 'ohmyfetch'
 
 export const createPrompt = async (prompt: string): Promise<string> => {
-  const { urls } = await $fetch<{ urls: { get: string } }>('/api/create', {
+  const { urls } = await $fetch<{ urls: { get: string } }>('https://kodaborieren.netlify.app/api/create', {
     method: 'POST',
     body: JSON.stringify({ prompt }),
   })
+
+  console.log(urls)
   return urls.get
 }
 
 export const resolvePrompt = async (promptUrl: string): Promise<string> => {
-  const { output } = await $fetch<{ output: string[] }>(`/api/resolve`, {
+  const { output } = await $fetch<{ output: string[] }>(`https://kodaborieren.netlify.app/api/resolve`, {
     params: { value: promptUrl },
   })
   return output[0]
