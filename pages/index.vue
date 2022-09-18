991cb970e632731307bff244262104789b489da0
Matej yangwao <ybdaba@gmail.com>
2022-09-18 12:58:20 +0200

    add gpt3 copy

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,24 @@
       German word "Kollaborieren" [kollaboriéren], which in English means collaborate. Second Koda, which is emerging NFT marketpalce on Polkadot.
     </p>
     <p>
-      
+      Creating art was always individual pursuit. Adding collaborative flavour opens floodgates for anyone, especially
+      creating new prompt artists who can take moment on the chain to memo their moment as every time they went through neural
+      nets it's unique as they won't see. On top of it, we believe you can share revenue from this party moment with your
+      friends.
+      Other than that can create poster games where people can generate unique art for unique events in their life with
+      emotional connection to the prompt as collective.
+    </p>
+    <p>
+      We believe when we can engage it with poster games where people can contribute to each part where anyone can do each own part like shaping avatar, environment, actions and &c
+    </p>
+    <p>
+      We take this as unique opporunity open flood gates people who don't have proper access for prompt to become prompt artist till stuff is not regulated 
+    </p>
+    <p>
+      Bussines model behind idea is to have sort of credit system per word, where each person can buy credits to guarantee service sustainability for future players.
+    </p>
+    <p>
+      Co-ownership should be highlighted in way that each user will sign ther user prompt with their web3 address to preserve ownership per address if future revenue share from unique memo moment when was prompt assembled.
     </p>
   </div>
 </template>
