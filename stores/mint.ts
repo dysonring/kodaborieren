387b72ec1358635fd95d2cdd352f366626e4490e
Viki Val <viktorko99@gmail.com>
2022-09-18 06:28:18 +0200

    :zap: add default testing value

--- stores/mint.ts
+++ stores/mint.ts
@@ -4,8 +4,8 @@ import { resolvePrompt } from '~~/services/lambda'
 export const useMintStore = defineStore('mint', {
   state: () => {
     return {
-      prompt: '',
-      previewUrl: '',
+      prompt: 'green python snake head in hoodie, portrait, vaporwave, synthwave, neon, vector graphics, cinematic, volumetric lighting, f 8 aperture, cinematic eastman 5 3 8 4 film, photorealistic',
+      previewUrl: 'https://replicate.com/api/models/stability-ai/stable-diffusion/files/a9cde7c9-f02f-4876-92f5-255a1c43a3e1/out-0.png',
     }
   },
   
