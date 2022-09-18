77ebfa97f2378d4532075a90b0f983369059f5f6
Viki Val <viktorko99@gmail.com>
2022-09-18 06:03:55 +0200

    :zap: plugins they told you not to worry about

---
+++ plugins/polkadot.ts
@@ -0,0 +1,12 @@
+// import keyring from '@polkadot/ui-keyring'
+// import { cryptoWaitReady } from '@polkadot/util-crypto'
+
+// export default ({ isDev }): void => {
+//   cryptoWaitReady().then(() => {
+//     keyring.loadAll({
+//       ss58Format: 42,
+//       // type: 'sr25519',
+//       isDevelopment: isDev,
+//     })
+//   })
+// }
