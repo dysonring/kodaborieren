a96a2cacaf6cd8e2762b566e36dcaf7daf6e8034
Viki Val <viktorko99@gmail.com>
2022-09-18 02:13:15 +0200

    :zap: reactive text area emits updates

--- components/form/reactive-textarea.vue
+++ components/form/reactive-textarea.vue
@@ -4,8 +4,11 @@
 
 <script lang="ts" setup>
   import { NInput } from 'naive-ui'
+
   const value = ref('')
   const { $gun } = useNuxtApp()
+  const emit = defineEmits(['update:modelValue'])
+
   const copy = $gun.get('test').get('paste');
   copy.on((data) => { value.value = data });
 
@@ -13,6 +16,7 @@
   watch(value, (newValue) => {
     console.log(`x is ${newValue}`)
     copy.put(newValue)
+    emit('update:modelValue', newValue)
   })
   
   console.log('$gun',$gun)
