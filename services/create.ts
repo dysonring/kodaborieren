6047613f88905329e7ff96986a3d523d125a14a3
Viki Val <viktorko99@gmail.com>
2022-09-18 06:16:23 +0200

    :zap: pinning API

--- services/create.ts
+++ services/create.ts
@@ -1,4 +1,6 @@
+import { createMetadata } from '@kodadot1/minimark'
 import { ApiFactory, Extrinsic } from '@kodadot1/sub-api'
+import { pinJson } from './nftStorage'
 
 const BASILISK_URL = 'wss://rococo-basilisk-rpc.hydration.dev'
 const DOLPHIN_URL = 'wss://ws.rococo.dolphin.engineering'
@@ -7,4 +9,18 @@ async function createNonFungibleToken(collectionId: string, tokenId: string, met
   const api = await ApiFactory.useApiInstance(BASILISK_URL)
   const create = api.tx.nft.mint(collectionId, tokenId, metadata)
   return create
+}
+
+async function constructMetadata(name: string, description: string, imageHash: string, file: Blob): Promise<string> {
+  const meta = createMetadata(
+    name,
+    description,
+    imageHash,
+    '',
+    [],
+    'https://kodadot.xyz',
+    file.type
+  )
+
+  return pinJson(meta, name)
 }
