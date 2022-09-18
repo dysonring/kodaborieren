aeaa0718e262c1554177896da7b68072f71660b7
Viki Val <viktorko99@gmail.com>
2022-09-18 02:11:01 +0200

    :zap: Wrap Replicate into services

---
+++ services/lambda.ts
@@ -0,0 +1,16 @@
+import { $fetch } from 'ohmyfetch'
+
+export const createPrompt = async (prompt: string): Promise<string> => {
+  const { urls } = await $fetch<{ urls: { get: string } }>('/api/create', {
+    method: 'POST',
+    body: JSON.stringify({ prompt }),
+  })
+  return urls.get
+}
+
+export const resolvePrompt = async (promptUrl: string): Promise<string> => {
+  const { output } = await $fetch<{ output: string[] }>(`/api/resolve`, {
+    params: { value: promptUrl },
+  })
+  return output[0]
+}
